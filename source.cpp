538fdccff0409f082aa0984c437f01e8b3b35d12
InesMestres <imestres@udesa.edu.ar>
2025-04-02 00:52:05 -0300

    Agregue limites para parametros en las clases

--- source.cpp
+++ source.cpp
@@ -12,15 +12,40 @@ Time::tiempo(){
 }
 
 Time::tiempo(horas_tiempo){
+    if(horas_tiempo < 0 || horas_tiempo > 24){throw std::invalid_argument("EL valor de horas es inválido");}
     horas = horas_tiempo;
     minutos = 0;
+    segundos = 0;
+    referencia = ""
+    std::cout << std::setw(2) << std::setfill('0') << horas << "," << std::setw(2) << std::setfill('0') << minutos << "," << std::setw(2) << std::setfill('0') segundos << " " << referencia << std::endl;
+}
+
+Time::tiempo(minutos_tiempo){
+    if(minutos_tiempo < 0 || minutos_tiempo > 60){throw std::invalid_argument("EL valor de minutos es inválido");}
+    horas = 0;
+    minutos = minutos_tiempo;
     segundos = 0;
     referencia = ""
     std::cout << std::setw(2) << std::setfill('0') << horas << "," << std::setw(2) << std::setfill('0') << minutos << "," << std::setw(2) << std::setfill('0') segundos << " " << referencia << std::endl;
 
 }
 
+Time::tiempo(segundos_tiempo){
+    if(segundos_tiempo < 0 || segundos_tiempo > 60){throw std::invalid_argument("El valor de sengundos es inválido");}
+    horas = 0;
+    minutos = 0;
+    segundos = segundos_tiempo;
+    referencia = ""
+    std::cout << std::setw(2) << std::setfill('0') << horas << "," << std::setw(2) << std::setfill('0') << minutos << "," << std::setw(2) << std::setfill('0') segundos << " " << referencia << std::endl;
+
+}
+
 void Time::mostrar_info(horas_tiempo, minutos_tiempo, segundos_tiempo) const{
+    
+    if(horas_tiempo < 0 || horas_tiempo > 24){throw std::invalid_argument("EL valor de horas es inválido");}
+    if(minutos_tiempo < 0 || minutos_tiempo > 60){throw std::invalid_argument("EL valor de minutos es inválido");}
+    if(segundos_tiempo < 0 || segundos_tiempo > 60){throw std::invalid_argument("El valor de sengundos es inválido");}
+
     horas = horas_tiempo;
     minutos = minutos_tiempo;
     segundos = segundos_tiempo;
